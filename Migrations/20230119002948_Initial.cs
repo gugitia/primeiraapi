874692d4357efc2bf94877a06751a2c3d4d899ca
gustavo <gugitia@gmail.com>
2023-01-20 10:14:53 -0300

    primeira api

---
+++ Migrations/20230119002948_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApiDoCurso.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Documento = table.Column<string>(maxLength: 14, nullable: false),
+                    TipoFornecedor = table.Column<int>(nullable: false),
+                    Ativo = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedors");
+        }
+    }
+}
---
+++ Models/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiDoCurso.Models
+{
+    public class ApiDbContext :DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Fornecedor> Fornecedors { get; set;}
+    }
+}
